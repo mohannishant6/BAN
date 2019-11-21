877f10c3a868ea611d3ee075eec150cd5f86c00b
ZhaoJiaJin <zhaojin0905@gmail.com>
2019-11-21 15:53:16 +0000

    start script

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ session="work"
 tmux start-server
 
 # create a new tmux session, starting vim from a saved session in the new window
-tmux new-session -d -s $session "./sink/sink  -pda '127.0.0.1:9099' " #"vim -S ~/.vim/sessions/kittybusiness"
+tmux new-session -d -s $session "./sink/sink  -pda '10.6.50.85:64753' " #"vim -S ~/.vim/sessions/kittybusiness"
 sleep 1
 
 tmux split-window -h "./goreman -f Procfile3 start "
